701c867c1959ae3dfda0bfb3d78d2092f6e6c685
ehdus09 <dgsw0509@dgsw.hs.kr>
2025-05-12 20:20:40 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,3 @@
+king, queen, look, bshop, knight, phone = 1, 1, 2, 2, 2, 8
+Fking, Fqueen, Flook, Fbshop, Fknight, Fphone = map(int, input().split())
+print(king - Fking, queen - Fqueen, look-Flook, bshop-Fbshop, knight-Fknight, phone-Fphone)
