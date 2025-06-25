9868b6e1ecedd9116236f81f6a9279900fde98a7
이도연 <dgsw0509@dgsw.hs.kr>
2025-06-25 22:05:45 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,5 @@
+seminars = {"Algorithm":204, "DataAnalysis":207, "ArtificialIntelligence":302, "CyberSecurity":"B101", "Network":303, "Startup":501, "TestStrategy":105}
+n= int(input())
+for i in range(n):
+    seminar = input()
+    print(seminars[seminar])
